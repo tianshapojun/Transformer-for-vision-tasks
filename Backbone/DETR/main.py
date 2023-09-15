3d3ac654bd2457a9b619d0081d919fa8f4601361
tianshapojun <383033930@qq.com>
2023-09-15 14:48:26 +0800

    Update main.py

--- Backbone/DETR/main.py
+++ Backbone/DETR/main.py
@@ -34,28 +34,6 @@ def get_args_parser():
                         help="Intermediate size of the feedforward layers in the transformer blocks")
     return parser
 
-class NestedTensor(object):
-    def __init__(self, tensors, mask: Optional[Tensor]):
-        self.tensors = tensors
-        self.mask = mask
-
-    def to(self, device):
-        # type: (Device) -> NestedTensor # noqa
-        cast_tensor = self.tensors.to(device)
-        mask = self.mask
-        if mask is not None:
-            assert mask is not None
-            cast_mask = mask.to(device)
-        else:
-            cast_mask = None
-        return NestedTensor(cast_tensor, cast_mask)
-
-    def decompose(self):
-        return self.tensors, self.mask
-
-    def __repr__(self):
-        return str(self.tensors)
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser('DETR training and evaluation script', parents=[get_args_parser()])
     args = parser.parse_args()
