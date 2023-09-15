32f4db77501f68b4f1cf4c6e69f96dd56d603ca5
tianshapojun <383033930@qq.com>
2023-09-15 14:57:07 +0800

    Update utils.py

--- Backbone/DETR/utils.py
+++ Backbone/DETR/utils.py
@@ -1,5 +1,6 @@
 from typing import Optional, List
 import torch
+import torch.distributed as dist
 from torch import Tensor
 
 class NestedTensor(object):
@@ -23,3 +24,19 @@ class NestedTensor(object):
 
     def __repr__(self):
         return str(self.tensors)
+
+def is_dist_avail_and_initialized():
+    if not dist.is_available():
+        return False
+    if not dist.is_initialized():
+        return False
+    return True
+
+def get_rank():
+    if not is_dist_avail_and_initialized():
+        return 0
+    return dist.get_rank()
+
+
+def is_main_process():
+    return get_rank() == 0
